3d62ac8be938f9867d14e4e250c2876c181d7399
Jose Ortiz <programmers@dreamcpu.com>
2017-06-21 12:00:52 -0400

    seperaated css and made change requested
    seperated css files for better readability and made changes requested by
    client

--- js/bergenstem.js
+++ js/bergenstem.js
@@ -1,11 +1,61 @@
+
+var player;
 $('ul.nav li.dropdown').hover(function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
 }, function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
 
 $('ul.nav li.dropdown').on("hover","ul.nav li.dropdown",function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
 }, function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
+
+$('.carousel').on('slide.bs.carousel',function() {
+currentIndex = $('div.active').index() + 1;
+console.log(currentIndex);
+if(currentIndex == 4)
+{
+
+player.playVideo();
+
+}
+});
+
+
+
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+function onYouTubeIframeAPIReady() {
+
+player = new YT.Player('youtube', {   
+height: '600',
+width: '1659',
+videoId: 'n1hHr62viSU',
+events: {
+'onStateChange': onPlayerStateChange
+}
+});
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+switch(event.data){
+case 1:
+$('.carousel').carousel('pause');
+break;
+default:
+$('.carousel').carousel();
+break;
+}
+}
+function stopVideo() {
+player.stopVideo();
+}
